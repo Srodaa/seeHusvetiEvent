0819f55b333943cee824cc95ffb897d2a9910d02
Szabó Dávid <david030719@gmail.com>
2025-04-16 23:38:43 +0200

    display fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import "./Tablazat";
 import Tablazat from "./Tablazat";
+import image from "/map_easter.png";
 
 function App() {
   return (
-    <div>
-      <Tablazat />
+    <div className="flex bg-[#222] min-w-screen">
+      <div className="w-1/2">
+        <img src={image} alt="background" />
+      </div>
+      <div className="w-1/2">
+        <Tablazat />
+      </div>
     </div>
   );
 }
