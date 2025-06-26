06008454b2c86799bb292293723c2dd8d68463b7
Szabó Dávid <david030719@gmail.com>
2025-06-26 16:45:02 +0200

    style: modify styling at the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="bg-gradient-to-br from-slate-900 via-slate-900 to-slate-700 min-w-screen">
+    <div className="min-w-screen">
       <div className="flex flex-wrap flex-col md:flex-row min-h-screen min-w-screen content-center">
         <div className="flex-1 flex justify-center h-65">
-          <div className="rounded-full bg-gray-800 w-60 h-60 hover:w-62 hover:h-62 flex items-center justify-center hover:shadow-[0px_0px_130px] shadow-cyan-500/50 duration-700">
+          <div className="rounded-full bg-[#343434] w-60 h-60 hover:w-62 hover:h-62 flex items-center justify-center duration-500 circle">
             <Link to="./auto">
-              <Car size="170" />
+              <Car size="170" color="black" />
             </Link>
           </div>
         </div>
         <div className="flex-1 flex justify-center h-65">
-          <div className="rounded-full bg-gray-800 w-60 h-60 hover:w-62 hover:h-62 flex items-center justify-center hover:shadow-[0px_0px_130px] shadow-cyan-500/50 duration-500">
+          <div className="rounded-full bg-[#343434] w-60 h-60 hover:w-62 hover:h-62 flex items-center justify-center duration-500 circle">
             <Link to="./husvet">
               <img src="src/assets/easter-egg.svg" width="170" />
             </Link>
