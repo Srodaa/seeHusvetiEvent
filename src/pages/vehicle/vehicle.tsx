4a4a2f9cdf6c4286d706c0c242df925ab7d0b39e
Szabó Dávid <david030719@gmail.com>
2025-06-30 22:55:01 +0200

    refactor: rename files

---
+++ src/pages/vehicle/vehicle.tsx
@@ -0,0 +1,19 @@
+type autoType = {
+  nev: string;
+  ppar: number;
+};
+
+const autok: autoType[] = [
+  {
+    nev: "Bandito",
+    ppar: 32000
+  },
+  {
+    nev: "Bergkraft B1000",
+    ppar: 7500
+  },
+  {
+    nev: "Blackstone Ridge Sport",
+    ppar: 6000
+  }
+];
