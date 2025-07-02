57980943493babd01ffac34e7105702ffe6052ee
Szabó Dávid <david030719@gmail.com>
2025-07-02 17:34:55 +0200

    fix: homebutton Link fix

--- src/components/ui/homebutton.tsx
+++ src/components/ui/homebutton.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export function HomeButton() {
   return (
     <div>
-      <Link to="../SrodaaSee/">
+      <Link to="../seeWeboldal/">
         <Button
           variant="outline"
           className="fixed top-5 left-2 z-50 bg-slate-850 border border-gray-700 hover:bg-slate-800 hover:text-white cursor-pointer"
