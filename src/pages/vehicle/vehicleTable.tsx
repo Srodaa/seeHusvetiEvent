fb4483def41a2a7f22d4544607e24ffee598ac38
Szabó Dávid <david030719@gmail.com>
2025-07-02 16:23:37 +0200

    feat: calculator for vehicle prices according to PP exchange rate

--- src/pages/vehicle/vehicleTable.tsx
+++ src/pages/vehicle/vehicleTable.tsx
@@ -2,21 +2,35 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { Vehicles as vehicles } from "./vehicleList";
 import { useState } from "react";
 import { VehicleSuggestion } from "./vehicleSuggestion";
-import { TuningTypes, VehicleTunings } from "./vehicleTunings";
+import { VehicleTunings } from "./vehicleTunings";
+import { Input } from "@/components/ui/input";
 
 export function VehicleTable() {
   const [filter, setFilter] = useState("");
   const [selectedCarPPPrice, setSelectedCarPPPrice] = useState(0);
   const [totalTuningPrice, setTotalTuningPrice] = useState<{ [key: number]: number }>({});
   const [selectedVehicleIndices, setSelectedVehicleIndices] = useState<number[]>([]);
+  const [ppExchange, setPpExchane] = useState(650);
 
   const filtered = vehicles.filter((veh) => veh.name.toLowerCase().includes(filter.toLowerCase()));
+  const totalPPPrice = selectedCarPPPrice + Object.values(totalTuningPrice).reduce((sum, price) => sum + price, 0);
+  const totalPrice = totalPPPrice * ppExchange;
 
   return (
     <div className="border-2 border-gray-600 rounded-md h-[70%] flex">
       <div className="flex-1">
-        <div className="mt-7 mx-10 max-w-120">
-          <VehicleSuggestion vehicles={vehicles} filter={filter} setFilter={setFilter} />
+        <div className="grid grid-cols-3 mt-7 mx-10 max-w-165">
+          <div className="col-span-2">
+            <VehicleSuggestion vehicles={vehicles} filter={filter} setFilter={setFilter} />
+          </div>
+          <Input
+            placeholder="Enter PP price, ex. 650"
+            className="ml-5 float-right"
+            type="number"
+            onChange={(e) => {
+              setPpExchane(parseInt(e.target.value) || 0);
+            }}
+          ></Input>
         </div>
         <div className="h-full max-w-250">
           <div className="border-2 rounded-md border-gray-600 m-10 overflow-auto relative max-h-4/6">
@@ -35,8 +49,8 @@ export function VehicleTable() {
                   return (
                     <TableRow key={i}>
                       <TableCell>{vehicles.name}</TableCell>
-                      <TableCell>{vehicles.price} $</TableCell>
-                      <TableCell>{vehicles.ppprice} PP</TableCell>
+                      <TableCell>{vehicles.price.toLocaleString("hu-HU")} $</TableCell>
+                      <TableCell>{vehicles.ppprice.toLocaleString("hu-HU")} PP</TableCell>
                       <TableCell>{vehicles.limit}</TableCell>
                       <TableCell>
                         <input
@@ -64,6 +78,10 @@ export function VehicleTable() {
                 })}
               </TableBody>
             </Table>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="text-xl font-bold">Total PP price: {totalPPPrice} PP</div>
+            <div className="text-xl font-bold">Total price: {totalPrice.toLocaleString("hu-HU")} $</div>
           </div>
         </div>
       </div>
@@ -103,7 +121,9 @@ export function VehicleTable() {
                   </TableBody>
                 </Table>
               </div>
-              <div className="mt-2 font-semibold">Total PP price: {totalTuningPrice[vehicleIndex] || 0} PP</div>
+              <div className="mt-2 font-semibold">
+                Total PP price: {totalTuningPrice[vehicleIndex].toLocaleString("hu-HU") || 0} PP
+              </div>
             </div>
           ))}
         </div>
